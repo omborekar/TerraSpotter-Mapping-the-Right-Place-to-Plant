618e789a1d27fa1b692548a4f5dfc9d239c75624
omborekar <omborekar18@gmail.com>
2025-08-31 13:48:36 +0530

    your update message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route  path="/" element={<Main />} />
+        <Route path="/" element={<Navbar />} />
+        
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
---
+++ frontend/src/components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <main style={{ padding: '2rem', textAlign: 'center' }}>
+            <h1>Welcome to Main Component</h1>
+        </main>
+    );
+};
+
+export default Main;
