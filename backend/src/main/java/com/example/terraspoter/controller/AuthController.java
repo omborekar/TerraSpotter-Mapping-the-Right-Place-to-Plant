b9bc5715590b0fd8cb1bdac54a64b840e6cc9b43
Om Borekar <omborekar18@gmail.com>
2026-03-27 21:05:38 +0530

    added route for ./

--- backend/src/main/java/com/example/terraspoter/controller/AuthController.java
+++ backend/src/main/java/com/example/terraspoter/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.example.terraspoter.payload.LoginRequest;
 import com.example.terraspoter.payload.SignupRequest;
 import com.example.terraspoter.service.AuthService;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,7 +18,8 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
-
+    @Value("${ML_API_URL}")
+    private String mlApiBaseUrl;
     private final AuthService authService;
 
     public AuthController(AuthService authService) {
